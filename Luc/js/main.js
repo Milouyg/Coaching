87b456b39950a1c39a69f4e361c60d515fffe9aa
darkluc05 <34852@ma-web.nl>
2023-03-30 16:50:12 +0200

    render finish

--- Luc/js/main.js
+++ Luc/js/main.js
@@ -46,14 +46,18 @@ class QuestionItem {
     constructor(newQuestionList) {
         this.questionList = newQuestionList;
 
+
         this.question = document.createElement("li");
         this.question.classList = "vragen__vraag vragen__vraag--active";
 
         this.questionInhoud = document.createElement("section");
         this.questionInhoud.classList = "vragen__inhoud";
 
+        this.questionNumber = document.createElement("h3");
+        this.questionNumber.classList = "vragen__nummer";
         this.questionParagraaf = document.createElement("p");
         this.questionParagraaf.classList = "vragen__paragraaf";
+        this.questionParagraaf.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates assumenda, aspernatur facilis maiores minima soluta voluptatum harum mollitia ab culpa at iure perspiciatis vitae quasi, quo doloremque fugiat est cumque."
 
         this.slider = document.createElement("div");
         this.slider.classList = "vragen__slider";
@@ -65,13 +69,7 @@ class QuestionItem {
         this.sliderNumbers = document.createElement("ul");
         this.sliderNumbers.classList = "vragen__sliderNumbers";
         
-        this.renderSliderNumbers();
         
-
-        
-
-        console.log(this.questionList)
-        this.renderQuestionNumber();
 
         // <input type="range" name="" id="" min="1" max="6">
     }
@@ -82,6 +80,7 @@ class QuestionItem {
         this.question.appendChild(this.slider);
         this.slider.appendChild(this.sliderInput)
         this.slider.appendChild(this.sliderNumbers);
+        this.renderSliderNumbers();
     }
     renderSliderNumbers(){
         for(let i = 0; i < 6; i++){
@@ -91,19 +90,55 @@ class QuestionItem {
         }
     }
     
-    renderQuestionNumber = () =>{
-        for (let i = 0; i < 1; i++) {
-            this.questionNumber = document.createElement("h3");
-            this.questionNumber.classList = "vragen__nummer";
-            this.questionNumber.innerText = i + 1;
-            this.questionInhoud.appendChild(this.questionNumber)
-        } 
+    
+}
+
+
+class Uitleg{
+    uitlegElement;
+    uitlegInhoud;
+    questionlist;
+    constructor(newQuestionList){
+        this.questionlist = newQuestionList;
+        this.uitlegElement = document.createElement("li");
+        this.uitlegElement.classList = "vragen__uitleg";
+
+        this.uitlegInhoud = document.createElement("p");
+        this.uitlegInhoud.classList = "vragen__paragraafUitleg";
+        this.uitlegInhoud.innerText =   "per vraag kan geklikt worden en dan komt er een balk onderaan de vraag tevoorschijn 1 voor niet van toepassing tot 6 altijd van toepassing. 3 is zo nu en dan op mij van toepassing. antwoord op gevoel waar jij het fijnste bij voelt."
+        console.log(this.uitlegElement)
+    }
+
+    render(){
+        this.questionlist.appendChild(this.uitlegElement)
+        this.uitlegElement.appendChild(this.uitlegInhoud)
     }
 }
 
 
 
-const questionlist = new QuestionList();
-questionlist.render();
-const questionItem = new QuestionItem(questionlist.questionList);
-questionItem.render();
+class App{
+    questionList;
+    constructor(){
+        this.questionlist = new QuestionList();
+        this.questionlist.render();
+        const uitleg = new Uitleg(this.questionlist.questionList);
+        uitleg.render()
+        this.renderItems()
+        
+    }
+
+    renderItems(){
+        for(let i = 0; i < 40; i++){
+            const questionItem = new QuestionItem(this.questionlist.questionList);
+            questionItem.render();
+        }
+    }
+    renderQuestionNumber = () =>{
+        for (let i = 0; i < 1; i++) {
+            this.questionNumber.innerText = i + 1;
+            this.questionInhoud.appendChild(this.questionNumber)
+        } 
+    }
+}
+const app = new App()
