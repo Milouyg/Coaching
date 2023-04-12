9d3acfda5a568dcec5f12b588ba6bad0694725a4
Nabil <33580@ma-web.nl>
2023-04-12 09:49:36 +0200

    kalendar grotendeels af

--- nabil/js/main.js
+++ nabil/js/main.js
@@ -1,23 +1,102 @@
-const calendar = document.getElementById("js--calendar");
-const modal = document.querySelectorAll(".modal");
-const dates = document.querySelectorAll('.dagboek__listitem'); 
-const x = document.querySelectorAll('.modal__x'); 
+document.addEventListener("DOMContentLoaded", function() {
+  const listItems = document.querySelectorAll(".dagboek__listitem");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const saveButton = document.querySelector(".modal__save");
+  const cancelButton = document.querySelector(".modal__cancel");
+  const input1 = document.querySelector("#modal__input1");
+  const input2 = document.querySelector("#modal__input2");
+  const input3 = document.querySelector("#modal__input3");
+  const prevMonthButton = document.querySelector(".dagboek__selectli--first");
+  const nextMonthButton = document.querySelector(".dagboek__selectli--third");
+  const monthName = document.querySelector(".dagboek__selectli--second");
+  let currentMonth = new Date().getMonth();
 
-dates.forEach(date => {
-  date.addEventListener('click', function() {
-    modal.forEach(modalItem => {
-      modalItem.classList.add('modal--on');
-    });
-    calendar.style.display = "none";
-  });
+  function generateDates(month) {
+    const dateList = document.querySelector(".dagboek__list");
+    dateList.innerHTML = "";
+
+    const daysInMonth = new Date(new Date().getFullYear(), month + 1, 0).getDate();
+    for (let i = 1; i <= daysInMonth; i++) {
+      const date = new Date(new Date().getFullYear(), month, i);
+      const listItem = document.createElement("li");
+      listItem.classList.add("dagboek__listitem");
+      listItem.innerHTML = `<div class="dagboek__dag">${i}</div>`;
+      dateList.appendChild(listItem);
+
+      listItem.addEventListener("click", () => {
+        showModal(listItem);
+      });
+    }
+
+    monthName.textContent = currentMonthName();
+  }
+
+  function showModal(listItem) {
+    const date = listItem.querySelector(".dagboek__dag").textContent;
+    modal.querySelector(".modal__date").textContent = date + " " + currentMonthName();
+    modal.classList.add("show");
+    overlay.classList.add("show");
+
+    const savedData = JSON.parse(localStorage.getItem(`data-${date}-${currentMonth}`));
+    if (savedData) {
+      input1.value = savedData.input1;
+      input2.value = savedData.input2;
+      input3.value = savedData.input3;
+    } else {
+      input1.value = "";
+      input2.value = "";
+      input3.value = "";
+    }
+  }
+
+  function saveData() {
+    const date = modal.querySelector(".modal__date").textContent.split(" ")[0];
+    const data = {
+      input1: input1.value,
+      input2: input2.value,
+      input3: input3.value
+    };
+    localStorage.setItem(`data-${date}-${currentMonth}`, JSON.stringify(data));
+    closeModal();
+  }
+
+  function closeModal() {
+    input1.value = "";
+    input2.value = "";
+    input3.value = "";
+    modal.classList.remove("show");
+    overlay.classList.remove("show");
+  }
+
+  function currentMonthName() {
+    const months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    const year = new Date().getFullYear();
+    return months[currentMonth] + " " + year;
+  }
+
+  function prevMonth() {
+    if (currentMonth > 0) {
+      currentMonth--;
+    } else {
+      currentMonth = 11;
+    }
+    generateDates(currentMonth);
+  }
+
+  function nextMonth() {
+    if (currentMonth < 11) {
+      currentMonth++;
+    } else {
+      currentMonth = 0;
+    }
+    generateDates(currentMonth);
+  }
+
+  prevMonthButton.addEventListener("click", prevMonth);
+  nextMonthButton.addEventListener("click", nextMonth);
+  saveButton.addEventListener("click", saveData);
+  cancelButton.addEventListener("click", closeModal);
+
+  generateDates(currentMonth);
 });
-
-x.onclick = function(){
-    modal.style.display = "none";
-    calendar.style.display = "block"
-}
-
-  
-
-var input = document.getElementById('saveServer').value;
-localStorage.setItem('server', input);
